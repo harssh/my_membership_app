dac4eb9de82ca99ecee11238dfc8eff570397cf7
Harssh <harssh@headfitted.com>
2013-10-18 20:40:18 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyMembershipApp::Application.config.secret_token = '08bb2fc46724da19aa0c651059ccc9ab6d55c7cb5dccbcc43cb62a39f3b51478f69c138873f33421cb6760a2d6d4afdb4d74d10bcd64daee10e840cb5b94e622'
